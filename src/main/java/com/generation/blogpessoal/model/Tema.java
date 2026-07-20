515f8064e924657fee85d8858fba998ac5db662f
Joao-Pedro-Duo <joaopedroduo09@gmail.com>
2026-07-20 14:21:24 -0300

    :sparkles: Criação da Entidade Tema e suas funcionalidades

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,43 @@
+package com.generation.blogpessoal.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_temas") // CREATE TABLE tb_temas();
+public class Tema {
+
+	@Id // PRYMARY KEY 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+	private Long id;
+	
+	@NotBlank(message = "O atributo descricão é obrigatório!")
+	@Size(min = 5, max = 1000, message = "A descricão deve ter no mínimo 05 e no máximo 1000 caracteres!")
+	@Column(length = 1000)
+	private String descricao;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
